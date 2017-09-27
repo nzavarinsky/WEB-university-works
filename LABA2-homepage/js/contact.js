5407ee540bc66910b2cf8a5a0561c712c148a709
nzavarinsky <nazarko1805>
2017-09-27 21:58:33 +0300

    [+] Full redesign + contact page
    Angular -

---
+++ LABA2-homepage/js/contact.js
@@ -0,0 +1,81 @@
+
+//PARALLAX ON BIO SECTION
+/*$(document).ready(function() {
+  var origheight = $("#bio").height();
+  var height = $(window).height();
+  if (height > origheight) {
+    $("#bio").height(height);
+  }
+  
+  $(window).scroll(function(){
+    var x = $(this).scrollTop();
+    $('#bio').css('background-position','center -'+parseInt(x/15)+'px');
+});  
+});*/
+
+
+
+//SMOOTH SCROLL ON NAVBAR CLICKS
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+//TV SET CHANNEL FLIP
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      1500);
+});
+
+
+// NAV BAR CLOSE ON PAGE CLICK
+$(document).ready(function () {
+    $(".navbar-nav li a").click(function(event) {
+        $(".navbar-collapse").collapse('hide');
+    });
+})
+
+
+//Fade header on scroll
+$(window).scroll(function(){
+    $(".main-title").css("opacity", 1 - $(window).scrollTop() / 500);
+    $(".hr-size").css("opacity", 1 - $(window).scrollTop() / 500);
+    $(".fa-chevron-down").css("opacity", 1 - $(window).scrollTop() / 500);
+  });
+
+
+
+
+$(document).ready(function() {
+  $('.grid').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'iframe',
+  // other options
+     gallery:{
+    enabled:true
+  }
+});
+});
+
+
+window.sr = ScrollReveal();
+sr.reveal('.section-header');
+sr.reveal('.effect-sarah');
+sr.reveal('.bio-text');
+sr.reveal('.about-item');
+sr.reveal('.contact-wrapper');
--- LABA2-homepage/js/index.js
+++ LABA2-homepage/js/index.js
@@ -1,121 +1,64 @@
-var app = angular.module('portfolio', [])
-
-    //navigation controller
-    .controller('navCtrl', function($scope) {
-
-
-        // Highlight the top nav as scrolling occurs
-        $('body').scrollspy({
-            target: '.navbar-fixed-top'
-        })
-
-        // Closes the Responsive Menu on Menu Item Click
-        $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
-            $('.navbar-toggle:visible').click();
-        });
-
-        //animation on Scroll
-        $scope.animateHeader = function() {
-            var doc = document,
-                elem = doc.documentElement,
-                header = doc.querySelector('.navbar-inverse'),
-                didScroll = false,
-                changeEffect = 220;
-
-            window.addEventListener('scroll', function(e) {
-                if (!didScroll) {
-                    didScroll = true;
-                    setTimeout($scope.scrollPage, 250);
-                }
-            }, false);
-
-            $scope.scrollPage = function() {
-                var sy = window.scrollY;
-                if (sy >= changeEffect) {
-                    $('.navbar-inverse').addClass('navbar-animated');
-                } else {
-                    $('.navbar-inverse').removeClass('navbar-animated');
-                }
-                didScroll = false;
-            };
-        };
-
-        $scope.animateHeader();
-    })
-
-    //home controller
-    .controller('homeCtrl', function($scope) {
-        $scope.title = "Порівняльне цивільне судочинство з правом Європейського Союзу";
-        $scope.description = "Доц. Коссак С. М.";
-    })
-
-    //about controller
-    .controller('aboutCtrl', function($scope) {
-        $scope.aboutText = {
-            title: "Про нас",
-            copyText1: `Мета: засвоєння студентами правових норм, теоретичних і практичних знань та
-    вмінь з порівняльного аналізу цивільної процесуальної форми відкриття, розгляду,
-    вирішення та перегляду цивільних справ за процесуальним законодавством України
-    та держав ЄС, на прикладі ФРН, Англії та Франції, ознайомлення з інститутом
-    судового доказування, основними учасниками цивільного процесу.
-    Предмет: цивільне судочинство України та кріаїн Європейського Союзу, зокрема
-    ФРН, Англії та Франції.`,
-            copyText2: `Зміст: Cудові системи України та держав ЄС (ФРН, Франція, Англія). Цивільне
-    процесуальне право України та держав ЄС. Учасники цивільного процесу.
-    Порівняльна характеристика. Судові докази. Відкриття провадження у цивільних
-    справах. Порівняльна характеристика. Попередній судовий розгляд. Судовий
-    розгляд цивільних справ. Судові рішення. Перегляд судових рішень`,
-            copyText3: `Місце у навчальному процесі – обов’язкова, 1 семестр.
-    У результаті вивчення дисципліни «Порівняльне цивільне судочинство з правом
-    Європейського Союзу» здобувач вищої освіти повинен:`,
-            copyText4: `
-    - знати:
-    - судові системи України та держав ЄС (ФРН, Англії, Франції);
-    - джерела цивільного процесуального законодавства України та держав ЄС щодо
-    відкриття, розгляду, вирішення та перегляду цивільних справ; судового
-    доказування, основних учасників цивільного процесу;
-    - порівняльну характеристику окремих визначених інститутів процесуального права;
-    - питання судової практики щодо тлумачення і застосування законодавства.
-   `,
-            copyText5: ` -вміти: - порівнювати та аналізувати процесуальні норми і формулювати власну позицію;
-    - застосовувати правові норми для вирішення конкретних практичних ситуацій
-    (казусів), узагальнювати матеріали судової практики, виявляти прогалини правового
-    регулювання і подавати пропозиції щодо вдосконалення процесуального
-    законодавства.
-    Декан проф. Бурдін`,
-            facebookUrl: "",
-            githubUrl: "",
-            codepenUrl: ""
-        };
-
-    })
-
-    .controller('newsCtrl', function($scope) {
-        $scope.newsect = {
-            title: `Новини`
-        };
-
-        $scope.works = [{
-            id: 1,
-            thumbnail: 'http://placehold.it/360x400',
-            url: '',
-            caption: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
-            title: 'Новина 1'
-        }, {
-            id: 2,
-            thumbnail: 'http://placehold.it/360x400',
-            url: '',
-            caption: 'Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.',
-            title: 'Новина 2'
-        }, {
-            id: 3,
-            thumbnail: 'http://placehold.it/360x400',
-            url: '',
-            caption: 'Nullam id dolor id nibh ultricies vehicula ut id elit.',
-            title: 'Новина 1'
-        }];
-    })
-    //contact controller
-    .controller('contactCtrl', function($scope) {
-        $scope.contact = {};
+//SMOOTH SCROLL ON NAVBAR CLICKS
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
     });
+  });
+
+  
+  //TV SET CHANNEL FLIP
+  $(function(){
+      $('.fadein img:gt(0)').hide();
+      setInterval(function(){
+        $('.fadein :first-child').fadeOut()
+           .next('img').fadeIn()
+           .end().appendTo('.fadein');}, 
+        1500);
+  });
+  
+  
+  // NAV BAR CLOSE ON PAGE CLICK
+  $(document).ready(function () {
+      $(".navbar-nav li a").click(function(event) {
+          $(".navbar-collapse").collapse('hide');
+      });
+  })
+  
+  
+  //Fade header on scroll
+  $(window).scroll(function(){
+      $(".main-title").css("opacity", 1 - $(window).scrollTop() / 500);
+      $(".hr-size").css("opacity", 1 - $(window).scrollTop() / 500);
+      $(".fa-chevron-down").css("opacity", 1 - $(window).scrollTop() / 500);
+    });
+  
+  
+  
+  
+  $(document).ready(function() {
+    $('.grid').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'iframe',
+    // other options
+       gallery:{
+      enabled:true
+    }
+  });
+  });
+  
+  
+  window.sr = ScrollReveal();
+  sr.reveal('.section-header');
+  sr.reveal('.effect-sarah');
+  sr.reveal('.bio-text');
+  sr.reveal('.about-item');
+  sr.reveal('.contact-wrapper');
