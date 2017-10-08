768224b47b73f83e28cc816bed32de89a137c6f2
nzavarinsky <nazarko1805>
2017-10-08 12:11:28 +0300

    [^] All pages
    [LAB 5]

--- LABA2-homepage/js/comments.js
+++ LABA2-homepage/js/comments.js
@@ -1,15 +1,41 @@
-$(".btnp").click(function(){
-    var commcontent = $("#field").val();
+$(document).ready(function() {
+    $('button').click(function() {
+        var comment = $('.commentBox').val();
+        $('<li>').text(comment).prependTo('.comments');
+        $('button').attr('disabled', 'true');
+        $('.counter').text('140');
+        $('.commentBox').val('');
+        var name = $('.Name').val();
+        $('<strong><h1>').text(comment).prependTo('.comments');
 
-  if(commcontent ==""){
-alert("Fuck U DumbAss, Write Something In TextBox.");
+        $('button').attr('disabled', 'true');
+        $('.counter').text('140');
+        $('.commentBox').val('');
+        $('.Name').val('');
+        var Date = $('.Date').val();
+        $('<strong><h1>').text(comment).prependTo('.comments');
 
-  }
-  else{
-    
-$(".comments").append("<div><h1>"+commcontent+"</h1></div>");
-  
-  $("#field").val('');
-  
-  }
-}); 
+        $('button').attr('disabled', 'true');
+        $('.counter').text('140');
+        $('.commentBox').val('');
+        $('.Date').val('');
+
+    });
+
+    $('.commentBox').keyup(function() {
+        var commentLength = $(this).val().length;
+        var charLeft = 140 - commentLength;
+        $('.counter').text(charLeft);
+
+        if (commentLength == 0) {
+            $('button').attr('disabled', 'true');
+        } else if (commentLength > 140) {
+            $('button').attr('disabled', 'true');
+        } else {
+            $('button').removeAttr('disabled', 'true');
+        }
+    });
+
+    $('button').attr('disabled', 'true');
+
+});
