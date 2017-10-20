e1266283c63ebff33c61d2b5fd60ebcdce610636
Nazar <nazarko1805@gmail.com>
2017-10-20 22:08:21 +0300

    [+] Lab8 (EventListeners/AngularJs LocalSotrage)

--- LABA2-homepage/js/test.js
+++ LABA2-homepage/js/test.js
@@ -33,6 +33,40 @@ app.controller('MainCtrl', function($scope, srvShareData, $location) {
     }
 });
 
+app.run(function($window, $rootScope) {
+    $rootScope.online = navigator.onLine;
+    $window.addEventListener("offline", function() {
+        $rootScope.$apply(function() {
+            $rootScope.online = false;
+        });
+    }, false);
+    $window.addEventListener("online", function() {
+        $rootScope.$apply(function() {
+            $rootScope.online = true;
+        });
+    }, false);
+
+    if ($rootScope.online = true) {
+        return mydata;
+    }
+
+    if ($rootScope.online = false) {
+        var KEY = 'SelectedValue';
+        var mydata = $window.localStorage.getItem(KEY);
+        if (mydata) {
+            mydata = JSON.parse(mydata);
+        } else {
+            mydata = [];
+        }
+        $window.localStorage.setItem(KEY, JSON.stringify(mydata));
+        var mydata = $window.localStorage.getItem(KEY);
+        if (mydata) {
+            mydata = JSON.parse(mydata);
+        }
+        return mydata;
+    }
+});
+
 app.controller('SecondCtrl', function($scope, srvShareData) {
 
     $scope.sharedData = srvShareData.getData();
@@ -43,23 +77,24 @@ app.service('srvShareData', function($window) {
     var KEY = 'SelectedValue';
 
     var addData = function(newObj) {
-        var mydata = $window.sessionStorage.getItem(KEY);
+        var mydata = $window.localStorage.getItem(KEY);
         if (mydata) {
             mydata = JSON.parse(mydata);
         } else {
             mydata = [];
         }
         mydata.push(newObj);
-        $window.sessionStorage.setItem(KEY, JSON.stringify(mydata));
+        $window.localStorage.setItem(KEY, JSON.stringify(mydata));
     };
 
     var getData = function() {
-        var mydata = $window.sessionStorage.getItem(KEY);
+        var mydata = $window.localStorage.getItem(KEY);
         if (mydata) {
             mydata = JSON.parse(mydata);
         }
         return mydata;
     };
+
 
     return {
         addData: addData,
